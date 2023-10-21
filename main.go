a66007be6a12e555f0ec49bbcca982a32f018aab
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-21 16:40:40 +0100

    Web hello world

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main;
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 );
 
 const PORT = ":8000";
 
+func index(w http.ResponseWriter, r *http.Request) {
+  io.WriteString(w, "<h1>Hello, world!</h1>")
+}
+
 func main() {
   fmt.Printf("Listening on http://localhost%s\n", PORT);
+
+  http.HandleFunc("/", index);
+
   log.Fatal(http.ListenAndServe(PORT, nil));
 }
