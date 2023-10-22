a7654a285bc5c0dbf81fe6f7381b25790169cf88
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-23 00:50:45 +0100

    Create an endpoint for adding a film

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+	"time"
 );
 
 type Film struct {
@@ -14,8 +15,29 @@ type Film struct {
 
 const PORT = ":8000";
 
+func addFilm(writer http.ResponseWriter, reader *http.Request) {
+  time.Sleep(1 * time.Second);
+
+	title    := reader.PostFormValue("title");
+	director := reader.PostFormValue("director");
+
+  // If either of the fields are empty, do nothing.
+  if len(title) == 0 || len(director) == 0 {
+    return;
+  }
+
+  fmt.Printf("Adding film:\n  %s - %s", title, director);
+
+	htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director);
+  tmpl, _ := template.New("t").Parse(htmlStr);
+
+  tmpl.Execute(writer, nil);
+}
+
 func index(writer http.ResponseWriter, reader *http.Request) {
-  template := template.Must(template.ParseFiles("index.html"));
+  fmt.Println("Returning main page.");
+
+  template := template.Must(template.ParseFiles("static/index.html"));
 
   films := map[string][]Film {
     "Films": {
@@ -31,7 +53,11 @@ func index(writer http.ResponseWriter, reader *http.Request) {
 func main() {
   fmt.Printf("Listening on http://localhost%s\n", PORT);
 
+  http.HandleFunc("/add-film/", addFilm);
   http.HandleFunc("/", index);
+
+  fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
   log.Fatal(http.ListenAndServe(PORT, nil));
 }
