a635f2b96b5484c69ce2e438eedadf5fcc3218d2
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-23 02:31:06 +0100

    Load data from a database

--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"text/template"
 	"time"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" // or postgres, sqlite3, etc.
+	"github.com/joho/godotenv"
 );
 
 type Film struct {
@@ -27,7 +33,7 @@ func addFilm(writer http.ResponseWriter, reader *http.Request) {
   // Simulate expensive task.
   time.Sleep(1 * time.Second);
 
-  fmt.Printf("Adding film:\n  %s - %s", title, director);
+  log.Printf("Adding film:\n                     └ %s - %s", title, director);
 
 	htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director);
   tmpl, _ := template.New("t").Parse(htmlStr);
@@ -36,23 +42,62 @@ func addFilm(writer http.ResponseWriter, reader *http.Request) {
 }
 
 func index(writer http.ResponseWriter, reader *http.Request) {
-  fmt.Println("Returning main page.");
+  username := os.Getenv("USERNAME");
+  password := os.Getenv("PASSWORD");
+  ip := os.Getenv("IP");
+  port := os.Getenv("PORT");
+  database := os.Getenv("DATABASE");
+
+  connectUrl := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, ip, port, database);
+
+  db, err := sql.Open("mysql", connectUrl);
+
+  if err != nil {
+    log.Fatalf("could not connect to database: %v", err);
+  }
+
+  log.Println("Connected to database.");
 
   template := template.Must(template.ParseFiles("static/index.html"));
 
-  films := map[string][]Film {
-    "Films": {
-      {Title: "The Godfather", Director: "Fancis Ford Coppola"},
-      {Title: "Blade Runner", Director: "Ridley Scott"},
-      {Title: "The Thing", Director: "John Carpenter"},
-    },
+  rows, err := db.Query("SELECT Title, Director FROM Films");
+
+  if err != nil {
+    log.Fatal(err);
+  }
+  
+  defer rows.Close();
+
+  films := make(map[string][]Film);
+
+  for rows.Next() {
+    var film Film;
+    
+    err = rows.Scan(&film.Title, &film.Director);
+
+    if err != nil {
+      log.Fatal(err);
+    }
+
+    // Append the film to the slice
+    films["Films"] = append(films["Films"], film);
+  }
+
+  err = rows.Err();
+
+  if err != nil {
+    log.Fatal(err);
   }
 
   template.Execute(writer, films);
 }
 
 func main() {
-  fmt.Printf("Listening on http://localhost%s\n", PORT);
+  if godotenv.Load() != nil {
+    log.Fatal("Failed to load .env");
+  }
+
+  log.Printf("Listening on http://localhost%s\n", PORT);
 
   http.HandleFunc("/add-film/", addFilm);
   http.HandleFunc("/", index);
